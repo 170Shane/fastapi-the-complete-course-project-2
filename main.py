d5fff2d8c224b97073727a69dcb462f29df29816
Shane Clark <170shane@gmail.com>
2025-10-07 18:18:59 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+# from routers.books_get_methods import router as books_router
+# from routers.books_post_methods import router as post_router
+# from routers.books_put_methods import router as put_router
+# from routers.books_delete_methods import router as delete_router
+
+app = FastAPI()
+
+# app.include_router(books_router)
+# app.include_router(post_router)
+# app.include_router(put_router)
+# app.include_router(delete_router)
