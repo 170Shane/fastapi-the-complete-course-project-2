a111b138fcdc84a29912e3bffd3d743a91e1e05f
Shane Clark <170shane@gmail.com>
2025-10-10 08:24:53 +0100

    Add Pydantic

--- models/book.py
+++ models/book.py
@@ -1,15 +1,22 @@
 from pydantic import BaseModel
 
-class Book(BaseModel):
-    id : str
+class Book():
+    id : str 
     title : str
     author : str 
     description : str
     rating : int
 
-    # def __init__(self, id: str, title: str, author: str, description: str, rating: int):
-    #     self.id = id
-    #     self.title = title
-    #     self.author = author
-    #     self.description = description
-    #     self.rating = rating
+    def __init__(self, id: str, title: str, author: str, description: str, rating: int):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+class BookRequest(BaseModel):
+    id : str 
+    title : str
+    author : str 
+    description : str
+    rating : int
--- routers/books2_post_methods.py
+++ routers/books2_post_methods.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Body
-from models.book import Book
+from models.book import Book, BookRequest
 from data.data import books
 router = APIRouter()
 
 
-@router.post("/create_book/", response_model=Book)
-async def create_book(book_request: Book):
-    books.append(book_request)
-    return book_request
+@router.post("/create_book/")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    books.append(new_book)
+    return {"message": "Book created successfully", "book": new_book}
