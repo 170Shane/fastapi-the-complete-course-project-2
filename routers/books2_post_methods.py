cf7dec77d7f993f023079fce4b9911b067c96e61
Shane Clark <170shane@gmail.com>
2025-10-12 11:21:12 +0100

    Create ID for new book

--- routers/books2_post_methods.py
+++ routers/books2_post_methods.py
@@ -7,5 +7,15 @@ router = APIRouter()
 @router.post("/create_book/")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    books.append(new_book)
+    books.append(find_book_index(new_book)) 
     return {"message": "Book created successfully", "book": new_book}
+
+def find_book_index(book: Book):
+    # if len(books) == 0:
+    #     book.id = 1
+    # else:
+    #     book.id = max(int(book.id) for book in books) + 1 if books else 0     # select maximum id from books and increment by 1
+
+    book.id = 1 if len(books) == 0 else max(int(book.id) for book in books) + 1
+
+    return book
