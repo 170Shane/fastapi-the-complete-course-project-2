f3b17eab02779a36079af7eae681f3d140d2656d
Shane Clark <170shane@gmail.com>
2025-10-18 07:48:28 +0100

    Refactor update_book function to raise HTTPException for not found books

--- routers/books2_put_methods.py
+++ routers/books2_put_methods.py
@@ -1,5 +1,5 @@
 
-from fastapi import Body,APIRouter
+from fastapi import Body,APIRouter, HTTPException
 from models import book
 from models.book import Book, BookRequest
 from data.data import books
@@ -12,11 +12,10 @@ router = APIRouter()
 # Example: Updating an existing book entry
 @router.put("/books/update_book/")
 async def update_book(book: BookRequest):
+    book_changed = False
     for idx, b in enumerate(books):        
             if b.id == book.id:
                 books[idx] = book                
-            return {"message": "Book updated successfully", "book": book}
-    return {"message": "Book not found"}
-    
-    
-        
+                book_changed = True            
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Book not found")
